fbee03462dbb2e0649c9eed2fc57eff81b4aecaf
Marcos Soares <marcosjmsoares@gmail.com>
2025-09-16 14:05:43 -0300

    inclusão de arquivos loguru

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,6 +1,11 @@
+from loguru import logger
+
+logger.add("meu_app.log")
+
 def soma(x,y):
-    print(x)
-    print(y)
+    logger.info(x)
+    logger.info(y)
+    logger.info(x + y)
     return x + y
 
 print(soma(2,3))
