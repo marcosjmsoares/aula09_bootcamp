a0e6214fca52a00aa811b0040eff8bdb0aa41714
Marcos Soares <marcosjmsoares@gmail.com>
2025-09-17 10:20:02 -0300

    ajuste scripts

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,6 +1,26 @@
 from loguru import logger
+from sys import stderr 
 
-logger.add("meu_app.log")
+
+logger.add("meu_app.log", level="CRITICAL")
+
+logger.add(
+                "meu_arquivo_de_logs_critical.log",
+                format="{time} {level} {message} {file}",
+                level="ERROR"
+            )
+
+logger.add(
+                "meu_arquivo_de_logs_critical.log",
+                format="{time} {level} {message} {file}",
+                level="INFO"
+            )
+
+logger.add(
+                "meu_arquivo_de_logs_critical.log",
+                format="{time} {level} {message} {file}",
+                level="CRITICAL"
+            )
 
 def soma(x,y):
     logger.info(x)
