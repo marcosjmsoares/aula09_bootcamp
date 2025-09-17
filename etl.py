a96ddd4a9d0145e16b7aba4a561346c84a11ec6d
Marcos Soares <marcosjmsoares@gmail.com>
2025-09-17 17:55:33 -0300

    adicionado outros arquivos decorator

--- etl.py
+++ etl.py
@@ -2,8 +2,10 @@ import pandas as pd
 import os 
 import glob
 import pandera as pa
+from utils_log import log_decorator
 
 #um funcao de extract que le e consolida no json
+@log_decorator
 def extrair_dados_e_consolidar(pasta: str) -> pd.DataFrame:
     arquivo_json = glob.glob(os.path.join(pasta,'*.json'))
     df_list = [pd.read_json(arquivo) for arquivo in arquivo_json]
@@ -17,6 +19,7 @@ def extrair_dados_e_consolidar(pasta: str) -> pd.DataFrame:
 
 # uma funcao que transforma
 
+@log_decorator
 def calcular_kpi_de_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame: 
     df["Total"] = df["Quantidade"] * df["Venda"]
     return df
@@ -24,6 +27,7 @@ def calcular_kpi_de_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
 
 # uma funcao que da load em csv ou parquet
 
+@log_decorator
 def carregar_dados(df: pd.DataFrame, formato_saida: list):
     """
     parametro que vai ser ou "csv" ou "parquet" ou "os dois"
@@ -36,6 +40,7 @@ def carregar_dados(df: pd.DataFrame, formato_saida: list):
     return carregar_dados
 
 
+@log_decorator
 def pipeline_calcular_kpi_de_vendas_consolidado(pasta: str, formato_de_saida: list):
     data_frame = extrair_dados_e_consolidar(pasta)
     data_frame_calculado = calcular_kpi_de_total_de_vendas(data_frame)
--- exemplo_01.py
+++ exemplo_01.py
@@ -1,10 +1,13 @@
 from utils_log import log_decorator
-
+from timer_decorator import time_measure_decorator
+import time
 #pydantic
 #pandera
 
 @log_decorator
+@time_measure_decorator
 def soma(x,y):
+    time.sleep(5)
     return x + y
 
 print(soma(2,3))
